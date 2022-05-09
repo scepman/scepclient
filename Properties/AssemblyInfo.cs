d56fcd47ec2b9a0694d22846998a5de58e0a8c0a
Christoph <christoph.hannebauer@glueckkanja-gab.com>
2022-05-09 12:10:30 +0200

    Updated Assembly properties

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ScepClientConsole")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ScepClientConsole")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTitle("ScepClient")]
+[assembly: AssemblyDescription("A client for the Simple Certificate Enrollment Protocol (SCEP)")]
+[assembly: AssemblyConfiguration("For .NET Framework 4.6.2")]
+[assembly: AssemblyCompany("glueckkanja-gab")]
+[assembly: AssemblyProduct("SCEPman")]
+[assembly: AssemblyCopyright("Copyright © 2022 glueckkanja-gab AG")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.*")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
